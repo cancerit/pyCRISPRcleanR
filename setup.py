331616bc9eb835a6d7a0b764b90c957be878f654
Shriram Bhosle <sb43@sanger.ac.uk>
2018-04-05 10:16:00 +0100

    added R travis in packages

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
     'author_email': 'pyhelp@sanger.ac.uk',
     'python_requires': '>= 3.3',
     'setup_requires': ['pytest','pytest-cov'],
-    'install_requires': ['logging','rpy2','pandas'],
+    'install_requires': ['logging','rpy2','pandas', 'numpy'],
     'packages': ['pyCRISPRcleanR'],
     'package_data': {'pyCRISPRcleanR':['config/*.conf']},
     'entry_points': {
