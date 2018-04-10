59ed75040f0cba99f3af7fbb169a5414694fb9e3
Shriram Bhosle <sb43@sanger.ac.uk>
2018-04-10 15:36:35 +0100

    user lib set

--- Rsupport/libInstall.R
+++ Rsupport/libInstall.R
@@ -10,7 +10,7 @@ ipak <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg))
     biocLite(new.pkg, ask=FALSE, lib=instLib)
-    .libPaths()
+    .libPaths(c(.libpaths(), instLib))
   sapply(pkg, library, character.only = TRUE)
 }
 
