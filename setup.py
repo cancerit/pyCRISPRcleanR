0dfea8faf5d13a71a7eab0911add21583e3e338a
Shriram Bhosle <sb43@sanger.ac.uk>
2018-04-16 15:05:36 +0100

    modified travis config to install from setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
     'author_email': 'cgphelp@sanger.ac.uk',
     'python_requires': '>= 3.3',
     'setup_requires': ['pytest','pytest-cover'],
-    'install_requires': ['rpy2','pandas', 'numpy', 'plotly'],
+    'install_requires': ['rpy2', 'pandas', 'numpy', 'plotly'],
     'packages': ['pyCRISPRcleanR'],
     'package_data': {'pyCRISPRcleanR':['config/*.conf']},
     'entry_points': {
