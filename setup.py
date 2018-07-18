72c6644c94c32367bfcb05cdda6ffef5810b1c88
Shriram Bhosle <sb43@sanger.ac.uk>
2018-07-18 16:49:10 +0100

    added mageck to setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,9 @@ config = {
     'url': 'https://github.com/CancerIT/pyCRISPRcleanR',
     'author_email': 'cgphelp@sanger.ac.uk',
     'python_requires': '>= 3.3',
-    'setup_requires': ['pytest','pytest-cover'],
-    'install_requires': ['rpy2', 'pandas', 'numpy', 'plotly', 'tzlocal'],
+    'setup_requires': ['pytest','pytest-cover', 'radon'],
+    'install_requires': ['rpy2', 'pandas', 'numpy', 'plotly', 'mageck', 'tzlocal'],
+    'find_links' : 'https://sourceforge.net/projects/mageck/files/0.5/mageck-0.5.7.tar.gz/download',
     'packages': ['pyCRISPRcleanR'],
     'package_data': {'pyCRISPRcleanR':['config/*.conf','segmentation/*.py']},
     'entry_points': {
